a6e603ff2f28d0e2c044a1de1089a6e6ac39a14a
yedilsan <yedilsanatovv@gmail.com>
2023-05-10 13:22:24 +0600

    added exercises

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,9 @@ const prisma = new PrismaClient();
 import * as categorys from './phrases/index.json';
 import * as phrasebank from './phrases/phraseBank.json';
 import * as phrases from './phrases/phrases.json';
+import * as exerciseCategorys from './phrases/exerciseCategory.json';
+import * as exerciseSubCategorys from './phrases/exerciseSubCategory.json';
+import * as exercises from './phrases/exercise.json';
 
 async function main() {
   const exists = await prisma.category.findUnique({ where: { id: 1 } });
@@ -27,6 +30,43 @@ async function main() {
   if (!exists3) {
     phrases.forEach(
       async (phrase) => await prisma.phrase.create({ data: phrase }),
+    );
+  }
+  // exercise category
+  const existsExerciseCategory = await prisma.exerciseCategory.findUnique({
+    where: { id: 1 },
+  });
+  console.log(exists);
+
+  if (!existsExerciseCategory) {
+    exerciseCategorys.forEach(
+      async (exerciseCategory) =>
+        await prisma.exerciseCategory.create({ data: exerciseCategory }),
+    );
+  }
+  // exercise sub category
+  const existsSubExerciseCategory = await prisma.exerciseSubCategory.findUnique(
+    {
+      where: { id: 1 },
+    },
+  );
+  console.log(exists);
+
+  if (!existsSubExerciseCategory) {
+    exerciseSubCategorys.forEach(
+      async (exerciseSubCategory) =>
+        await prisma.exerciseSubCategory.create({ data: exerciseSubCategory }),
+    );
+  }
+  // exercise
+  const existsExercise = await prisma.exercise.findUnique({
+    where: { id: 1 },
+  });
+  console.log(exists);
+
+  if (!existsExercise) {
+    exercises.forEach(
+      async (exercise) => await prisma.exercise.create({ data: exercise }),
     );
   }
 }
